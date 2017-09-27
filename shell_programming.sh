d74639b5cd25e79de7bb8ea605d09056891d6f39
takuzuka@mail.ru <MaksimsVecenans@gmail.com>
2017-09-27 11:52:09 +0300

    20170927_11_55

--- shell_programming.sh
+++ shell_programming.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
+#4. piemērs
+echo"tiek pildits skripts: "$0
+#echo $o
+#echo $n
+echo "Skriptam nodoto argumentu skaits: "$#
+echo "Skriptam nodoti argumenti (attelošana/grupēšana 1):"$*
+echo "Skriptam nodoti argumenti (attelošana/grupēšana 2):"$@
+#echo $?
+echo "Skriptam piešķirtais procesa numurs: "$$
+#echo $!
 
-NAME="Vaards Uzvaards"
+#3. piemērs
+#NAME="Vaards Uzvaards"
+#echo $NAME
+#unset NAME
 #echo $NAME
 
-
+#2. piemērs
+#NAME="Vaards Uzvaards"
 #readonly NAME
 #echo $NAME
 #NAME="Maks V"
 #echo $NAME
 
-echo $NAME
-unset NAME
-echo $NAME
+#1. piemērs
+#NAME="Vaards Uzvaards"
+#echo $NAME
