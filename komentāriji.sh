5b67300ad60b324c6ca3ae18d08cf92fb2c3e32a
Live session user <MaksimsVecenans@gmail.com>
2017-10-08 20:40:18 +0000

    20170910_17_40

---
+++ komentāriji.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "cat: nepieciešans failu lasīšanai"
+echo "cd: pārvietošanās starp direktorijam (cd "direktorijas nosaukums")"
+echo "chmod: maina privilēģijas failiem (chmod 744 "faila nosaukums")"
+echo "echo: rāda uzrakstīto tekstu (echo "teksts")"
+echo "history: rāda komandu vai darbību vēsturi"
+echo "ls: rāda failus, kuri atrodas direktorijā, kurā jūs atrodaties"
+echo "lsb_release: rāda svarīgu informāciju (jā tada ir)"
+echo "mkdir: veido failus"
+echo "man: skaidro komandas nozīmi (man "komandas nosaukums")"
+echo "nano: modificē failu jēb veido skriptu"
+echo "path: veido ceļu"
+echo "pwd: parāda tagadējo direktoriju"
+echo "rmdir: direktoriju dzēšana (rmdir "direktoriju nosaukums")"
+echo "uname vai uname -r: parāda sistēmas informāciju"
+echo "who vai whoami: parāda, kas tagad ir ielogojies sistēma"
+
+echo "Palīgpogas: tab vai bultiņas"
