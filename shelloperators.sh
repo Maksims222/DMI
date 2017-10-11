3314c901e44afbbe2f5e7b4dc48b3751476ac47a
takuzuka@mail.ru <MaksimsVecenans@gmail.com>
2017-10-11 11:52:25 +0300

    20171011_11_50

--- shelloperators.sh
+++ shelloperators.sh
@@ -1,6 +1,55 @@
 #!/bin/bash
 
+#5.piemers____
+
+if [ $# == 2 ]
+#ja if skriptam nodotu  argumentu skait($#) ir vienads(==)ar 2
+then
+# tad(then) izpildam sekojosas darbibas lidz ....
+a=$1
+b=$2
+
+val51=`expr $a + $b`
+echo "$a + $b =" $val51
+val52=`expr $a - $b`
+echo "$a - $b =" $val52
+val53=`expr $a \* $b`
+echo "$a * $b =" $val53
+val54=`expr $b / $a`
+echo "$b / $a =" $val54
+val55=`expr $b % $a`
+echo "$b % $a =" $val55
+fi
+
+if [ $a == $b ]
+then
+   echo " $a ir vienāds ar $b"
+fi
+
+if [ $a != $b ]
+then
+   echo "$a nav vienads ar  $b"
+#....lidz šim. 
+fi
+
+#4.piemers_operacijas ar argumentiem
+: <<'END'
+a=$1
+b=$2
+val41=`expr $a + $b`
+echo "$a + $b = "$val41
+val42=`expr $a - $b`
+echo "$a - $b = "$val42
+val43=`expr $a \* $b`
+echo "$a * $b = "$val43
+val44=`expr $a / $b`
+echo "$a / $b = "$val44
+val45=`expr $a % $b`
+echo "$a % $b = "$val45
+END
+
 #3.piemers- operacijas ar mainigajiem
+: <<'END'
 a=29
 b=15
 val31=`expr $a + $b`
@@ -13,11 +62,8 @@ val34=`expr $a / $b`
 echo "$a / $b = "$val34
 val35=`expr $a % $b`
 echo "$a % $b = "$val35
+END
 
-
-#1.piemers -saskaitisana
-#val=`expr 4 + 2`
-#echo "Total value : $val"
 
 #2.piemers - operacija ar konstante
 : <<'END'
@@ -33,4 +79,7 @@ val25=`expr 2 % 3`
 echo "2 % 3 = "$val25
 END
 
+#1.piemers -saskaitisana
+#val=`expr 4 + 2`
+#echo "Total value : $val"
 
